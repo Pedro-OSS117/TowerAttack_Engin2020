ba9357f410373ae563bcdb60b45ef899cb23c7da
Pedro-OSS117 <61086029+Pedro-OSS117@users.noreply.github.com>
2021-03-01 17:14:56 +0100

    1er Cours - MapManager Et Map Data

---
+++ Assets/Scripts/Datas/MapData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class MapData
+{
+    [SerializeField]
+    private int _width;
+
+    [SerializeField]
+    private int _height;
+
+    [SerializeField]
+    private SquareData[] _grid;
+
+    public MapData(int width, int height)
+    {
+        _grid = new SquareData[width * height];
+    }
+}
---
+++ Assets/Scripts/Datas/SquareData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public enum SquareState
+{
+    Normal,
+    Wall,
+    Water,
+}
+
+public enum Alignment
+{
+    Player,
+    IA,
+    Neutral
+}
+
+[Serializable]
+public struct SquareData
+{
+    [SerializeField]
+    private SquareState _state;
+    
+    [SerializeField]
+    private Alignment _alignment;
+}
