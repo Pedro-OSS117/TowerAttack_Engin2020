b5aff8bd2a35f41b6f6eaeb5c87b5bf61c1736ba
Pedro-OSS117 <pierreproudhon@gmail.com>
2021-03-15 13:14:37 +0100

    Entity Modele de données

---
+++ Assets/Scripts/Datas/Enity/ActionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ActionData
+{
+    [SerializeField]
+    private float _timeToDoAction = 0;
+
+    public float TimeToDoAction { get { return _timeToDoAction; } }
+}
---
+++ Assets/Scripts/Datas/Enity/EntityData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EntityType
+{
+    Unit,
+    Building
+}
+
+public class EntityData : MonoBehaviour
+{
+    [SerializeField]
+    private int _life = 0;
+
+    public int Life { get { return _life; } }
+
+    [SerializeField]
+    private string _nameEntity = "No Name";
+
+    public string Name { get { return _nameEntity; } }
+
+    [SerializeField]
+    private EntityType _type = EntityType.Unit;
+
+    public EntityType Type { get { return _type; } }
+
+    [SerializeField]
+    private int _popAmount = 0;
+
+    public int PopAmount { get { return _popAmount; } }
+
+    [SerializeField]
+    private ActionData[] _actions = null;
+
+}
